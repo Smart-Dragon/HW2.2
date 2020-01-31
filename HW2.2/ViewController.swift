da6e09dfdd2127a2e5af541ba26df83f4b21d3df
Aleksei <supportalex@rambler.ru>
2020-01-31 16:56:13 +0300

    add changeColor and refactoring

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     // MARK: - IBOutlets
     
@@ -33,6 +33,7 @@ class ViewController: UIViewController {
         
         setupColorView()
         setupSliders()
+        changeColor()
     }
     
     // MARK: - Private Methods
@@ -49,20 +50,41 @@ class ViewController: UIViewController {
     
     private func setupSlider(slider: UISlider, color: UIColor) {
         slider.minimumTrackTintColor = color
+        slider.setValue(Float.random(in: 0.0..<1.0), animated: false)
+    }
+    
+    private func changeColor() {
+        changeText()
+        
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+    
+    private func changeText() {
+        let redText = floatString(redSlider.value)
+        let greenText = floatString(greenSlider.value)
+        let blueText = floatString(blueSlider.value)
+        
+        redLabel.text = redText
+        greenLabel.text = greenText
+        blueLabel.text = blueText
+        
+        redTextField.text = redText
+        greenTextField.text = greenText
+        blueTextField.text = blueText
+    }
+    
+    private func floatString(_ value: Float) -> String {
+        return String(format: "%.2f", value)
     }
     
     // MARK: - IBActions
     
-    @IBAction func changeRedColorAction() {
-        redLabel.text = String(redSlider.value)
-    }
-    
-    @IBAction func changeGreenColorAction() {
-        greenLabel.text = String(greenSlider.value)
-    }
-    
-    @IBAction func changeBlueColorAction() {
-        blueLabel.text = String(blueSlider.value)
+    @IBAction func changeColorAction() {
+        changeColor()
     }
     
 }
